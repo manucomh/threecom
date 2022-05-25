377b16c073727201276275f0ea6e06275c8700f1
manucomh <pmaucayllehuaman@gmail.com>
2022-05-24 17:40:14 -0700

    arreglando el header

---
+++ index.php
@@ -0,0 +1,315 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta http-equiv="Content-Type" content="text/html,charset=utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="utf-8">
+	<title>Servicio Técnico | THREECOM SAC | Telf.: 990995652</title>
+	<link rel="canonical" href="https://www.threecom.com">
+	<meta property="url" content="Somos el servicio técnico autorizado">
+	<style>
+		@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&display=swap');
+		/*@import url('https://fonts.googleapis.com/css2?family=Alumni+Sans+Inline+One&family=Roboto:wght@100;300&display=swap');*/
+		*{
+			box-sizing: border-box;
+		}
+		div{
+			display: block;
+		}
+		body{
+			font-family: 'Roboto',sans-serif;
+			margin: 0;
+			padding: 0;
+		}
+	</style>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="public/css/threecom-main.css">
+</head>
+<body>
+	<div class="general_container">
+		<div class="conatiner_threecom_main">
+			<div class="container_header">
+				<div class="container_header_center">
+					<div class="content_block">
+						<div class="content_left">
+							<img src="public/images/threecomsac_logo.jpeg" class="threecomsac_logo_main">
+						</div>
+					</div>
+					<div class="content_block_rigth">
+						<div class="content_rigth">
+							<nav class="nav_content">
+								<ul class="ul_header">
+									<li class="li_header">
+										<a href="" class="li_link">
+											Inicio
+										</a>
+									</li>
+									<li class="li_header">
+										<a href="" class="li_link">
+											Sobre Nosotros
+										</a>
+									</li>
+									<li class="li_header">
+										<a href="" class="li_link">
+											Servicios
+										</a>
+									</li>
+									<li class="li_header">
+										<a href="" class="li_link">
+											Repuestos
+										</a>
+									</li>
+									<li class="li_header">
+										<a href="" class="li_link">
+											Contactos
+										</a>
+									</li>
+								</ul>
+								<div class="div_content_icon_email">
+									<a href="" class="li_link">
+										<span class="material-icons md-48 header_email_span">
+											email
+										</span>
+									</a>
+								</div>
+								<div class="div_menu_toggle">
+									<span class="material-icons md-48 header_menu_span">
+										menu
+									</span>
+								</div>
+							</nav>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+		<div class="container_body_threecom">
+			<div class="element-motion-effects-layer-container">
+				<div class="element-motion-effects-layer">
+					<div class="flex-vertical-center"> 
+						<h1>Nuestros</h1>
+						<h2>Servicios</h2>
+					</div>
+				</div>
+			</div>
+			<div class="second_container_body">
+				<h1 class="h1_tec">
+					SOPORTE TÉCNICO
+				</h1>
+				<h2>
+					ESPECIALIZADOS
+				</h2>
+				<br>
+				<p class="p_text_">
+					Brindamos soluciones tecnológicas que permiten a las empresas innovar, automatizar y desarrollarse.
+				</p><br>
+				<p class="p_text_">
+					Más de 10 años de experiencia respaldan nuestro trabajo, asesórese por los expertos en tecnología.
+				</p>
+
+				<div class="sections_elementors">
+					<section class="section_elements">
+						<div class="wrap_one div_wrap">
+							<div class="left_icon_wrap">
+								<img width="101" height="101" src="public/images/laptop.png" class="attachment-large size-large" alt="" loading="lazy">
+							</div>
+							<div class="right_text_wrap">
+								<div class="content_min_height">
+									<h1 class="h1_title_laptop">Laptop</h1>
+									<p class="p_text_description_min">
+										Contamos con laboratorios avanzados con las mejores herramientas, con una plataforma virtual, donde puedes hacer seguimiento del avance de la reparación a su portátil.
+									</p>
+								</div>
+								
+							</div>
+						</div>
+						<div class="wrap_two div_wrap">
+							<div class="left_icon_wrap">
+								<img width="101" height="101" src="public/images/impresora.png" class="attachment-large size-large" alt="" loading="lazy">
+							</div>
+							<div class="right_text_wrap">
+								<div class="content_min_height">
+									<h1 class="h1_title_laptop">Impresora</h1>
+									<p class="p_text_description_min">
+										Servicio de revisión y diagnóstico, reparación, mantenimiento preventivo y correctivo de impresoras de todas las marcas y modelos: laser, matriciales, multifunción, entre otras.
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="wrap_three div_wrap">
+							<div class="left_icon_wrap">
+								<img width="101" height="101" src="public/images/smart-tv.png" class="attachment-large size-large" alt="" loading="lazy">
+							</div>
+							<div class="right_text_wrap">
+								<div class="content_min_height">
+									<h1 class="h1_title_laptop">Smarth TV</h1>
+									<p class="p_text_description_min">
+										Realizamos el diagnóstico y mantenimiento de su Smart TV en las marcas más reconocida del Perú con un stock amplio de repuestos.
+									</p>
+								</div>
+							</div>
+						</div>
+					</section>
+					<section class="section_elements">
+						<div class="wrap_one div_wrap">
+							<div class="left_icon_wrap">
+								<img width="101" height="101" src="public/images/pc.png" class="attachment-large size-large" alt="" loading="lazy">
+							</div>
+							<div class="right_text_wrap">
+								<div class="content_min_height">
+									<h1 class="h1_title_laptop">PCs</h1>
+									<p class="p_text_description_min">
+										Contamos con un laboratorio informático y electrónico, con un staff de técnicos por ingenieros altamente capacitados para el mantenimiento de equipo informático.
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="wrap_two div_wrap">
+							<div class="left_icon_wrap">
+								<img width="101" height="101" src="public/images/cctv.png" class="attachment-large size-large" alt="" loading="lazy">
+							</div>
+							<div class="right_text_wrap">
+								<div class="content_min_height">
+									<h1 class="h1_title_laptop">CCTV</h1>
+									<p class="p_text_description_min">
+										Realizamos la instalación y el mantenimiento de cámaras de seguridad preventivo (DVR, NVR, Camáras), también optimizamos sistemas CCTV.
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="wrap_three div_wrap">
+							<div class="left_icon_wrap">
+								<img width="101" height="101" src="public/images/redes.png" class="attachment-large size-large" alt="" loading="lazy">
+							</div>
+							<div class="right_text_wrap">
+								<div class="content_min_height">
+									<h1 class="h1_title_laptop">Redes</h1>
+									<p class="p_text_description_min">
+										Gestión, administración y mantenimiento de redes informáticas e inalámbricas.
+									</p>
+								</div>
+							</div>
+						</div>
+					</section>
+				</div>
+			</div>
+
+			<div class="three_container_body">
+				<div class="wrap_container_center_div">
+					<h1 class="h1_partners">SOMOS PARTNER</h1>
+					<h2 class="h2_partners">DE LAS MEJORES MARCAS</h2>
+					<p class="p_text_partners">
+						Nuestra relación de negocios con grandes marcas, nos ha llevado a posicionarnos como una gran empresa en el área de soporte técnico. Ahora contamos con una relación comercial con la marca HP, LENOVO y ESET siendo partner de unas de las marcas mas reconocidas a nivel mundial, permitiéndonos ofrecer soluciones comerciales acorde a las necesidades de nuestros clientes.
+					</p>
+					<!-- <h3 class="h3_authorized">
+						SOMOS CENTRO AUTORIZADO DE SOPORTE TÉCNICO
+					</h3> -->
+					<div class="container_img_boxes_marcas">
+						<div class="div_content_imgs_marca">
+							<img width="164" height="68" src="public/images/marcas/logo-eset.png">
+						</div>
+						<div class="div_content_imgs_marca">
+							<img width="205" height="41" src="public/images/marcas/logo-lenovo.png">
+						</div>
+						<div class="div_content_imgs_marca">
+							<img width="60" height="61" src="public/images/marcas/logo-hp.png">
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="foor_container_body">
+				<div class="container_wrap_maxwidth_center">
+					<div class="class_flex_width_max">
+						<div class="section_container_column one_section">
+							<img src="public/images/THREECOM_SAC.png" width="150xpx" height="30px" class="img_logo_threecom_sac">
+							<br>
+							<p class="text_threecom">
+								En THREECOM SAC estamos dirigidos a prestar un servicio de calidad cumpliendo con los estándares de nuestros clientes.
+							</p>
+							<pl class="p_line_color_red_"></p>
+						</div>
+						<div class="section_container_column two_section">
+							<h2 class="explorer_navigation">EXPLORAR</h2>
+							<ul>
+								<li>
+									<a href="">Inicio</a>
+								</li>
+								<li>
+									<a href="">Sobre Nosotros</a>
+								</li>
+								<li>
+									<a href="">Servicios</a>
+								</li>
+								<li>
+									<a href="">Repuestos</a>
+								</li>
+								<li>
+									<a href="">Contacto</a>
+								</li>
+							</ul>
+							<pl class="p_line_color_red_"></p>
+						</div>
+						<div class="section_container_column three_section">
+							<h2 class="contact_threecom">CONTACTO</h2>
+							<ul>
+								<li>
+									<span><></span>
+									<span>Av Peru N° 234 Andahuaylas</span>
+								</li>
+								<li>
+									<span><></span>
+									<span>Av Peru N° 234 Andahuaylas</span>
+								</li>
+								<li>
+									<span><></span>
+									<span>soportethreecom@gmail.com</span>
+								</li>
+							</ul>
+							<br>
+							<p class="p_line_color_red_"></p>
+							<br>
+							<div class="container_number">
+								<img src="public/resources/phone-call.png">
+								<h3 class="strong_text_call">
+									<strong>cel: </strong>
+									<strong>990 995 652</strong><br>
+									<strong>24/7 (al Número)</strong>
+								</h3>
+							</div>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+
+		
+			<?php  
+
+				require_once('includes_html/footer.php');
+
+			?>
+		
+		</div>
+
+
+	<div class="btn_go_whatsapp">
+		<a class="whatsapp_go_link" href="https://api.whatsapp.com/send?phone=51989783198&text=Necesito%20un%20repuesto go-to-whatsapp" target="_blank">
+			<span class="material-icons md-48" >
+				whatsapp 
+			</span>
+			
+		</a>
+	</div>
+	<div class="btn_go_fb">
+		<a class="whatsapp_go_link" href="https://www.facebook.com/threecomsac/?go-to-fanpage-threecom" target="_blank">
+			<span class="material-icons md-48">
+				facebook
+			</span>
+		</a>
+	</div>
+</body>
+</html>
